478d0baf6d6a396580ffe4c03288db28c0f424a8
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2024-06-20 12:54:49 +0200

    refactor(qoe-metrics-dashboard): overview and detail models
    Differentiate between requests between consumption and metrics

--- qoe-metrics-dashboard/src/app/models/types/requests/metrics-details-request-params.type.ts
+++
@@ -1,4 +0,0 @@
-/**
- * The endpoint allows to filter based on the properties of the reception report and the QoE report
- */
-export type TMetricsDetailsRequestParams = Pick<any, 'reportTime' | 'clientID' | 'recordingSessionId'>;
