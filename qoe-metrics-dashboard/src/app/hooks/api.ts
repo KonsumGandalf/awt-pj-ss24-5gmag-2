17d12c5081fc02fada599cf781962f14b15f7158
Cedric Lenßen <cedric.lenssen@gmail.com>
2024-06-21 10:58:02 +0200

    remove disabled rule

--- qoe-metrics-dashboard/src/app/hooks/api.ts
+++ qoe-metrics-dashboard/src/app/hooks/api.ts
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import { isNil, omitBy } from 'lodash';
