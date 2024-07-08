f299d25979002de6559e40ebb5232ff448db0ab6
Cedric Lenßen <cedric.lenssen@gmail.com>
2024-07-08 19:45:44 +0200

    fix metrics reporting endpoint

--- express-mock-af/routes/metrics-reporting.js
+++ express-mock-af/routes/metrics-reporting.js
@@ -1,13 +1,20 @@
-var express = require('express');
-const Utils = require('../utils/Utils');
+var express = require("express");
+const Utils = require("../utils/Utils");
 var router = express.Router();
 
-router.post('/:provisioningSessionId/:metricsReportingConfigurationId', function (req, res, next) {
+router.post(
+  "/:provisioningSessionId/:metricsReportingConfigurationId",
+  function (req, res, next) {
     const payload = req.body;
-    const path = `${Utils.getReportPath(req.params.provisioningSessionId, true)}/${req.params.metricsReportingConfigurationId}/${new Date().toISOString()}.xml`;
+    const path = `public/reports/${
+      req.params.provisioningSessionId
+    }/metrics_reports/${
+      req.params.metricsReportingConfigurationId
+    }/${new Date().toISOString()}.xml`;
 
-    Utils.writeToDisk(path, payload, 'metrics');
+    Utils.writeToDisk(path, payload, "metrics");
     res.send(204);
-});
+  }
+);
 
 module.exports = router;
